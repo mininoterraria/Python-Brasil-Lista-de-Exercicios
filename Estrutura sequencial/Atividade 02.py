168b06f4ac170fe321ebf260d9c6c8720f711338
Diego Rufino <128259891+mininoterraria@users.noreply.github.com>
2024-11-13 21:45:51 -0300

    Create Atividade 02.py

---
+++ Estrutura sequencial/Atividade 02.py
@@ -0,0 +1,5 @@
+#02) Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+numero = int(input("Insira um número: "))
+
+print(f"O número informado foi: {numero} ")
