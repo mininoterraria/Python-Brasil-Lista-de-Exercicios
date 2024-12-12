e36fc3031a8a4f3d7268f15c132d50bfd5e24d96
Diego Rufino <128259891+mininoterraria@users.noreply.github.com>
2024-12-12 17:04:41 -0300

    Delete Praticando Python condicionais if,elif e else directory

--- Praticando Python condicionais if,elif e else/Atividade 05.py
+++
@@ -1,9 +0,0 @@
-#05) Carlos quer monitorar seu orçamento mensal para evitar gastos excessivos. Ele estabeleceu um limite de R$ 3.000,00 para seus gastos e precisa de um programa que ajude a controlar suas despesas. O programa deve receber o total de despesas realizadas e informar se ele ultrapassou o limite ou ainda está dentro do orçamento.
-despesa_mensal = float(input("Insira a despesa mensal: "))
-if(despesa_mensal < 0): #Se a despesa mensal for menor que 0, emitirá valor inválido,não executando o restante do código
-  print("Valor inválido!")
-else:
-  if(despesa_mensal > 3000):
-    print("Atenção! Você ultrapassou o limite de orçamento.")
-  else:
-    print("Está tudo nos conformes!")
--- Praticando Python condicionais if,elif e else/Atividade 09.py
+++
@@ -1,6 +0,0 @@
-#09) Lucas está desenvolvendo um jogo e precisa de uma funcionalidade que verifique se um número é par ou ímpar. Essa verificação será usada para definir ações diferentes dentro do jogo. Escreva um programa que receba um número inteiro e exiba uma mensagem informando se ele é par ou ímpar.
-numero = int(input("Insira um número inteiro qualquer: "))
-if(numero % 2 == 0):
-  print("É par")
-else:
-  print("É impar")
