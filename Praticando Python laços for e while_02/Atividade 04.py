ed3ca7b785d923f461a31c363c894b69ba1a4667
Diego Rufino <128259891+mininoterraria@users.noreply.github.com>
2024-12-12 17:06:47 -0300

    Add files via upload

---
+++ Praticando Python laços for e while_02/Atividade 04.py
@@ -0,0 +1,4 @@
+#04) Você está implementando uma funcionalidade no "Buscante" que exibe uma contagem regressiva de tempo para uma promoção especial. O objetivo é imprimir a contagem regressiva começando de 10 até 1 e, ao final, exibir a mensagem "Promoção iniciada!".
+for contagem in range(10,0,-1):
+  print(contagem)
+print("Promoção iniciada!")
