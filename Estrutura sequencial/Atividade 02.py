c5b71be491cfe489c8a8e7f906f22b29b93e60bb
Diego Rufino <128259891+mininoterraria@users.noreply.github.com>
2024-12-12 17:04:34 -0300

    Delete Estrutura sequencial directory

--- Estrutura sequencial/Atividade 02.py
+++
@@ -1,5 +0,0 @@
-#02) Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
-
-numero = int(input("Insira um número: "))
-
-print(f"O número informado foi: {numero} ")
--- Estrutura sequencial/Atividade 10.py
+++
@@ -1,9 +0,0 @@
-#10) Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
-#F = (°C x 1.8) + 32
-
-temperatura_celsius = float(input("Insira a temperatura em celsius: "))
-
-#Fórmula de conversão de celsius para fahrenheit.
-conversao_fahrenheit = (temperatura_celsius * 1.8) + 32
-
-print(f"A conversão de celsius para fahrenheit é: {conversao_fahrenheit}°F")
