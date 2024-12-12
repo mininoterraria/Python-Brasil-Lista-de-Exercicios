11798ca69faa6425b57f311cf1f685408a3dcfcd
Diego Rufino <128259891+mininoterraria@users.noreply.github.com>
2024-12-12 17:16:18 -0300

    Delete Estrutura Sequencial_01 directory

--- Estrutura Sequencial_01/Atividade 05.py
+++
@@ -1,8 +0,0 @@
-#05) Faça um Programa que converta metros para centímetros.
-
-metros = float(input("Insira a medida em metros: "))
-
-#Cálculo para conversão de metros em centímetros.
-conversao = metros * 100
-
-print(f"A conversão de metros para centímetros é: {conversao}cm")
--- Estrutura Sequencial_01/Atividade 09.py
+++
@@ -1,9 +0,0 @@
-#09) Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
-#C = 5 * ((F-32) / 9).
-
-temperatura_fahrenheit = float(input("Insira a temperatura em fahrenheit: "))
-
-#Fórmula de conversão de fahrenheit para celsius.
-conversao_celsius = 5 * ((temperatura_fahrenheit - 32) / 9)
-
-print(f"A conversão de fahrenheit para celsius é: {conversao_celsius}°C")
