f9486155c517a5395415f08928299e2660d620e7
Diego Rufino <128259891+mininoterraria@users.noreply.github.com>
2024-12-12 17:51:37 -0300

    Delete Praticando Python laços for e while_02 directory

--- Praticando Python laços for e while_02/Atividade 03.py
+++
@@ -1,6 +0,0 @@
-#03) Você está desenvolvendo um sistema de controle de estoque para o "Buscante". Um dos requisitos é verificar a quantidade de exemplares de um livro em estoque e continuar vendendo até que o estoque se esgote. Sempre que uma venda é realizada, o sistema deve informar o usuário e atualizar a quantidade disponível. Considere a seguinte variável inicial:
-estoque = 5
-while estoque > 0:
-  print(f"Estoque disponível: {estoque}")
-  estoque -= 1
-print("Livros vendidos!")
